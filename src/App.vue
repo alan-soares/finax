36dd7cdba3fefe6e9c00128845f9cd2affc48e27
Alan-Soares <alansoares@zipmail.com.br>
2019-09-14 19:12:50 -0300

    controle financeiro pessoal

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<v-app>
+   <template>
+  <div>
+    <v-app-bar
+      color="deep-purple accent-4"
+      dense
+      dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>Finax</v-toolbar-title>
+      <div class="flex-grow-1"></div>
+      <v-btn icon>
+        <v-icon>mdi-fingerprint</v-icon>
+      </v-btn>	
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+		<v-btn icon>
+       <v-icon>attach_money</v-icon>
+      </v-btn>
+	<v-btn icon>
+       	<v-icon> mdi-account</v-icon>
+      	</v-btn>
+
+      <v-menu
+        left
+        bottom
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item
+            v-for="n in 5"
+            :key="n"
+            @click="() => {}"
+          >
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
   </div>
 </template>
-
+</v-app>
+   
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    data: () => ({
+      height: 30,
+      lightsOut: false,
+      window: false,
+    }),
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
